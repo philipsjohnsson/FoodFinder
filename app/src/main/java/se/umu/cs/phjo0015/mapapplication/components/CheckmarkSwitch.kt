aa32cd91d12bb65b87d127aded4317dff0b060c7
Philip Jonsson <pj222qg@student.lnu.se>
2025-08-28 16:57:40 +0200

    fixed the visual for settings

--- app/src/main/java/se/umu/cs/phjo0015/mapapplication/components/CheckmarkSwitch.kt
+++ app/src/main/java/se/umu/cs/phjo0015/mapapplication/components/CheckmarkSwitch.kt
@@ -1,9 +1,12 @@
 package se.umu.cs.phjo0015.mapapplication.components
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 
 @SuppressLint("UnrememberedMutableState")
 @Composable
@@ -13,6 +16,7 @@ fun SwitchMinimalExample(
 ) {
 
     Switch(
+        modifier = Modifier.scale(0.7f),
         checked = isEnabled.value,
         onCheckedChange = { isChecked: Boolean ->
             toggleShowUserLocation()
--- app/src/main/java/se/umu/cs/phjo0015/mapapplication/pages/SettingsPage.kt
+++ app/src/main/java/se/umu/cs/phjo0015/mapapplication/pages/SettingsPage.kt
@@ -1,12 +1,22 @@
 package se.umu.cs.phjo0015.mapapplication.pages
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import se.umu.cs.phjo0015.mapapplication.components.SwitchMinimalExample
 import se.umu.cs.phjo0015.mapapplication.model.SettingsToggle
 
@@ -14,10 +24,27 @@ import se.umu.cs.phjo0015.mapapplication.model.SettingsToggle
 fun SettingsPage(
     settingToggles: List<SettingsToggle>
 ) {
-    Column {
+    Column (
+        Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            "Kartinställningar",
+            color = Color(0xFF497349),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
         for(setting in settingToggles) {
-            Row {
-                Text(setting.title)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    setting.title,
+                    style = MaterialTheme.typography.titleMedium
+                )
                 SwitchMinimalExample(setting.onToggle, setting.isEnabled)
             }
         }
